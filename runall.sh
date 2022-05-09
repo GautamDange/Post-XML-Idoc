4626a001de820ee100e1dbff8bed113efae35464
Gautam <gautam.dange2007@gmail.com>
2022-05-09 11:49:23 +0200

    initial Files

---
+++ runall.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+python run0.py
+python run1.py
+python run2.py
+python run3.py
+python run4.py
+
+python run5.py
+python run6.py
+python run7.py
+python run8.py
+python run90.py
